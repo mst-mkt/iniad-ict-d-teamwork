c8cacc4b67994081acead79dfeb76b11090c43f9
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-09-04 00:08:29 +0900

    add: basic styles

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+
+def index_view(request: HttpRequest) -> HttpResponse:
+    return render(request, "pages/index.html")
