98c63889a488d8753aecc8bd1f52d77ad1a68e28
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2024-09-05 13:42:51 +0900

    fix: shop_idのクエリの処理を修正

--- app/api/index.py
+++ app/api/index.py
@@ -24,8 +24,9 @@ def index_api(request: HttpRequest) -> HttpResponse:
     res_json = res.json()
 
     shops = res_json["results"]["shop"]
-    shop_ids = [f"shop_ids={shop["id"]}" for shop in shops]
+    shop_ids = [shop["id"] for shop in shops]
+    shop_id_queries = "&".join([f"shop_ids={shop_id}" for shop_id in shop_ids])
 
-    redirect_url = f"/select/?{'&'.join(shop_ids)}"
+    redirect_url = f"/select/?{shop_id_queries}"
 
     return redirect(redirect_url)
